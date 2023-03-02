78ff812b77f59b5b58c2dbc590faf7a67337efae
Federico Baez <federicko94@gmail.com>
2023-03-02 20:15:34 -0300

    fixed navbar user route

--- client/src/components/admin/Navbar.jsx
+++ client/src/components/admin/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li className="nav-item"><a className="nav-link" href="#!">Users</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/admin/users">Users</a></li>
                         <li className="nav-item"><a className="nav-link" aria-current="page" href="/admin/categories">Categories</a></li>
                         <li className="nav-item"><a className="nav-link" href="/admin/products">Products</a></li>
                     </ul>
